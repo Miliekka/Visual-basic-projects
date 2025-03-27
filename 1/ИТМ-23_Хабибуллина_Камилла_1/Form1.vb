4e684d0d1473647003587f398828f14306085eab
Miliekka <hey_mila@mail.ru>
2025-03-28 01:26:09 +0300

    мини проекты в visual studio

---
+++ 1/ИТМ-23_Хабибуллина_Камилла_1/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim enteredPassword As String = TextBoxPassword.Text
+        If enteredPassword = "secret" Then
+            Dim form2 As New Form2()
+            form2.Show()
+            Me.Hide()
+        Else
+            MessageBox.Show("Неправильный пароль")
+        End If
+    End Sub
+
+
+End Class
---
+++ 3/MessageBox/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+
+    Private Sub OpenFileToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenFileToolStripMenuItem.Click
+        Dim O As OpenFileDialog
+        OpenFileDialog1.ShowDialog()
+    End Sub
+
+    Private Sub CloseFileToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CloseFileToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub FontToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FontToolStripMenuItem.Click
+        Dim M As Font
+        FontDialog1.ShowDialog()
+        M = FontDialog1.Font
+        Me.Font = M
+    End Sub
+
+    Private Sub PrinterToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PrinterToolStripMenuItem.Click
+        Dim F As PrintDialog
+        PrintDialog1.ShowDialog()
+    End Sub
+
+    Private Sub ColorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ColorToolStripMenuItem.Click
+        Dim S As Color
+        ColorDialog1.ShowDialog()
+        S = ColorDialog1.Color
+        Me.BackColor = S
+    End Sub
+
+    Private Sub ServiceToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ServiceToolStripMenuItem.Click
+        Dim newName As String = InputBox("Введите новое имя формы")
+        Me.Text = newName
+        Dim i As Integer
+        i = MsgBox("Закрыть программу?", vbAbort.Abort, "Вывод")
+        If i = 6 Then End
+
+    End Sub
+End Class
