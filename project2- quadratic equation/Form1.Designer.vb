265b432cb4040677c70ac98eed540a831f585880
Miliekka <hey_mila@mail.ru>
2025-03-28 21:36:37 +0300

    Add files via upload

---
+++ project2- quadratic equation/Form1.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.White
+        Button1.Font = New Font("Segoe UI", 12F)
+        Button1.Location = New Point(232, 270)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(346, 56)
+        Button1.TabIndex = 0
+        Button1.Text = "Решить уравнение"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(460, 133)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(50, 23)
+        TextBox1.TabIndex = 1
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(460, 173)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(50, 23)
+        TextBox2.TabIndex = 2
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(460, 211)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(50, 23)
+        TextBox3.TabIndex = 3
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Stencil", 14.25F)
+        Label1.Location = New Point(287, 134)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(132, 22)
+        Label1.TabIndex = 4
+        Label1.Text = "Введите число A"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Stencil", 14.25F)
+        Label2.Location = New Point(287, 174)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(133, 22)
+        Label2.TabIndex = 5
+        Label2.Text = "Введите число B"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Stencil", 14.25F)
+        Label3.Location = New Point(287, 212)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(131, 22)
+        Label3.TabIndex = 6
+        Label3.Text = "Введите число C"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Stencil", 20.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label4.Location = New Point(287, 57)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(234, 32)
+        Label4.TabIndex = 7
+        Label4.Text = "Ax^2 + Bx + C = 0"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.MistyRose
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+
+End Class
---
+++ project2- quadratic equation/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Aa, Bb, Cc, Xx1, Xx2, Yy1, Errr, Dd As Single
+        Aa = TextBox1.Text
+        Bb = TextBox2.Text
+        Cc = TextBox3.Text
+        Yy1 = 0
+        Xx1 = 0
+        Xx2 = 0
+
+        Dd = Bb ^ 2 - 4 * Aa * Cc
+        If Dd > 0 Then
+            Xx1 = Math.Round((-Bb + Math.Sqrt(Dd)) / (2 * Aa), 1)
+            Xx2 = Math.Round((-Bb - Math.Sqrt(Dd)) / (2 * Aa), 1)
+            Call Abcx1x2(Aa, Bb, Cc, Xx1, Yy1, Dd, Errr)
+            MessageBox.Show($"В уравнении 2 корня: x1 = {Xx1}, x2 = {Xx2}")
+        ElseIf Dd = 0 Then
+            Xx1 = -Bb / (2 * Aa)
+            Call Abcx1x2(Aa, Bb, Cc, Xx1, Yy1, Dd, Errr)
+            MessageBox.Show($"В уравнении 1 корень: x1 = {Xx1}")
+        Else
+            MessageBox.Show("В уравнении нет корней")
+        End If
+    End Sub
+    Private Sub Abcx1x2(ByVal A As Single, ByVal B As Single, ByVal C As Single, ByRef x As Single,
+        ByRef y As Single, ByVal D As Single, ByRef ER As Integer)
+
+        y = A * x * x + B * x + C
+    End Sub
+
+
+End Class
